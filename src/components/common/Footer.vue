95dcae5a266407eda1d0560b87615a2434ebe8f9
Tune2000 <11282429+Tune2000@user.noreply.gitee.com>
2023-12-27 12:06:02 +0800

    小说网站前端代码

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,36 @@
+ <template>
+  <div class="footer">
+    <div class="box_center cf">
+      <div class="copyright">
+        <ul>
+          
+          <li>
+            所有小说和章节均由网友上传，转载只为宣传本书让更多读者欣赏
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+import { getToken } from "@/utils/auth";
+export default {
+  name: "Footer",
+  setup() {
+    const router = useRouter();
+    const goFeedBack = () => {
+      if(!getToken()){
+        router.push({name: 'login'});
+      }else{
+      router.push({name: 'feadback'});
+      }
+    };
+    return {
+      goFeedBack
+    }
+  },
+  
+};
+</script>
